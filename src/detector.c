f041138d7135c28a4f08537d124df8a07be1df59
vitoryeso <vitoryeso@outlook.com>
2020-05-16 19:45:24 -0300

    debug backup path

--- src/detector.c
+++ src/detector.c
@@ -30,7 +30,10 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     char *valid_images = option_find_str(options, "valid", train_images);
     char *backup_directory = option_find_str(options, "backup", "/backup/");
     printf(backup_directory);
+    char *wwwww = "/content/drive/My\ Drive/";
+    printf(wwwww)
     return;
+
 
     network net_map;
     if (calc_map) {
