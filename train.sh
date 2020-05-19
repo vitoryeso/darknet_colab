3bb6e13e4f7db5a6a274ee1ac041d352d4e3e91d
vitoryeso <vitoryeso@outlook.com>
2020-05-18 23:09:17 -0300

    add train.sh

--- train.sh
+++ train.sh
@@ -14,7 +14,7 @@ cfg=${STRS[0]}
 IFS=' '
 
 dir="${path}train_${cfg}"
-if [ "$3" == ""]; then
+if [ "$3" == "" ]; then
     num_trains=1
     mkdir "${dir}" 
     mkdir "${dir}/weights"
@@ -29,7 +29,8 @@ if [ "$3" == ""]; then
     wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.conv.137
     ./darknet detector train "$1" "$2" yolov4.conv.137 -dont_show > "${dir}/logs/${num_trains}_log.txt"
 else
-    num_trains=$(ls "${dir}/logs/" | grep -i "log" | wc -l) + 1
+    num_trains=$(ls "${dir}/logs/" | grep -i "log" | wc -l)
+    num_trains= "$num_trains + 1"
     echo "train ${num_trains}" >> "${dir}/info.txt"
     echo >> "${dir}/info.txt" 
     echo "$(date)" >> "${dir}/info.txt"
