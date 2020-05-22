e40b76264691c28404f195492f6bc4cefb1023e1
vitoryeso <vitoryeso@outlook.com>
2020-05-22 14:51:58 -0300

    debug saving path

--- src/detector.c
+++ src/detector.c
@@ -42,11 +42,8 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     luCopy(colab_directory, backup_directory, provlen);
     luReplaceChar(colab_directory, '$', ' ');
     strcat(colab_directory, "train_");
-    strcat(colab_directory, prov2[0]);
+//    strcat(colab_directory, prov2[0]);
     strcat(colab_directory, "/weights");
-    printf("len: %d \n", strlen(prov2[0])); 
-    for(int i=0;i<strlen(prov2[0]);i++) printf("%c \n", prov2[0][i]);
-    printf("%c \n", prov2[0][strlen(prov2[0])]);
     printf("Colab Directory: %s \n", colab_directory);
 
     network net_map;
